6f9f216073e5983b8fc8822147156417603e2670
Vishal Kumar <vishal.kumar@moondive.co>
2023-07-13 14:34:51 +0530

    change the UI IP

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,2 +1,3 @@
-export const API = "http://localhost:8521/api/v1"
+// export const API = "http://localhost:8521/api/v1"
+export const API = "http://3.108.191.165:8080/api/v1"
 export const USER = "http://localhost:8521/api/v1/user"
