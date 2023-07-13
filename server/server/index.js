33b40bd474dd25708e270256fa738f796aa46a0c
Vishal Kumar <vishal.kumar@moondive.co>
2023-07-13 14:28:25 +0530

    chnage DB to cluster

--- server/server/index.js
+++ server/server/index.js
@@ -2,9 +2,10 @@ import Config from "config";
 import Routes from "./routes";
 import Server from "./common/server";
 
-const dbUrl = `mongodb://${Config.get("databaseHost")}:${Config.get(
-  "databasePort"
-)}/${Config.get("databaseName")}`;
+// const dbUrl = `mongodb://${Config.get("databaseHost")}:${Config.get(
+//   "databasePort"
+// )}/${Config.get("databaseName")}`;
+const dbUrl = `mongodb+srv://vishalkumar:test@test.jqdwr3k.mongodb.net/`
 const server = new Server()
   .router(Routes)
   .configureSwagger(Config.get("swaggerDefinition"))
